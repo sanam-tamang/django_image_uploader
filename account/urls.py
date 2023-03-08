e13f5b9c8245a839362310e39d4566efe1b6a051
sanam-tamang <snm.tmg7@gmail.com>
2023-03-08 16:38:03 +0545

    added static files and changes in url and setting

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import NewsFeedView
 
 urlpatterns = [
-    path('newsfeed/', NewsFeedView.as_view())
+    path('newsfeed/get', NewsFeedView.as_view({"get": "get request"})),
+    path('newsfeed/post', NewsFeedView.as_view({"post": "post request"})),
 ]
--- drt_img_uploader/settings.py
+++ drt_img_uploader/settings.py
@@ -123,8 +123,24 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static/')
+]
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
